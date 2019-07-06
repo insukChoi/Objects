7c1c848c2f236a31e6a17f1a4b70b435b80f6c28
insukChoi <cis1725@gmail.com>
2019-07-06 19:44:00 +0900

    create Objects project

---
+++ TicketSales/src/com/insuk/Invitation.java
@@ -0,0 +1,9 @@
+package com.insuk;
+
+public class Invitation {
+    final static public Invitation EMPTY = new Invitation(null);
+    final private Theater theater;
+    public Invitation(Theater theater){
+        this.theater = theater;
+    }
+}
---
+++ TicketSales/src/com/insuk/TicketSeller.java
@@ -0,0 +1,19 @@
+package com.insuk;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+    public void setTicketOffice(TicketOffice ticketOffice){
+        this.ticketOffice = ticketOffice;
+    }
+    public Ticket getTicket(Audience audience){
+        Ticket ticket = Ticket.EMPTY;
+        if (audience.getInvitation() != Invitation.EMPTY){
+            ticket = ticketOffice.getTicketWithNoFee();
+            if (ticket != Ticket.EMPTY) audience.removeInvitation();
+        }else if(audience.hasAmount(ticketOffice.getTicketPrice())){
+            ticket = ticketOffice.getTicketWithFee();
+            if (ticket != Ticket.EMPTY) audience.minusAmount(ticketOffice.getTicketPrice());
+        }
+        return ticket;
+    }
+}
